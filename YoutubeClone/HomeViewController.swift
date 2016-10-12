05e704f8ee713a7da0e3312a42ed35493d1446d7
Ky Nguyen <nguyentruongky33@gmail.com>
2016-10-13 04:43:16 +0700

    - Init the project - Start the project with appDelegate - Change the ViewController to HomeViewController

---
+++ YoutubeClone/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Ky Nguyen on 10/13/16.
+//  Copyright © 2016 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Home"
+        collectionView?.backgroundColor = UIColor.red
+    }
+    
+    func numberOfItem() {
+        
+    }
+}
